4e4704147c5af3caf83a3c5e2e22e6b63e4f8230
Vinod Pillai <vinod.pillai@heliossolutions.co>
2024-09-02 17:21:10 +0530

    Updated as per the LangChain V0.2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
 from operator import itemgetter
-from langchain.schema import StrOutputParser
 
 import os
 from os.path import join, dirname
@@ -16,10 +16,13 @@ prompt1 = ChatPromptTemplate.from_template('Which country won the {game} world c
 prompt2 = ChatPromptTemplate.from_template('Suggest me the best {entity} of that {country}')
 
 # Model 
-llm = ChatOpenAI(api_key=os.environ.get('OPENAI_API_KEY'), temperature=0.5)
+llm = ChatOpenAI(api_key=os.environ.get('OPENAI_API_KEY'), model=os.environ.get('OPENAI_MODEL'), temperature=0.5) # type: ignore
+
+# Parser
+parser = StrOutputParser()
 
 # Chain
-chain1 = prompt1|llm|StrOutputParser()
-chain2 = ({'country':chain1, 'entity': itemgetter('entity')}|prompt2|llm|StrOutputParser())
+chain1 = prompt1|llm|parser
+chain2 = ({'country':chain1, 'entity': itemgetter('entity')}|prompt2|llm|parser)
 
 print(chain2.invoke({'game':'Cricket','entity':'food'}))
